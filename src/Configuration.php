1c2d4768d7cc9cb316fc8570bc598701e9e0095b
ugnsma <ugnsma@users.noreply.github.com>
2026-08-25 07:31:28 +0000

    feat: own downstream release workflows in the generator (#117)

--- src/Configuration.php
+++ src/Configuration.php
@@ -20,7 +20,7 @@ class Configuration
 
     protected string $host = 'https://api.mail.hostinger.com';
 
-    protected string $userAgent = 'hostinger-mail-php-sdk/1.17.2';
+    protected string $userAgent = 'hostinger-mail-php-sdk/1.18.0';
 
     protected bool $debug = false;
 
